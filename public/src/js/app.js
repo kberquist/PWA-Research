9fb2acda257c623d7544577b4670e363d85688da
Kevin Berquist <kberquist@gmail.com>
2020-05-02 12:57:35 -0700

    completed first caching section

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -11,36 +11,53 @@ if ('serviceWorker' in navigator){
     });
 } 
 
-window.addEventListener('beforeinstallprompt',function(event){
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;    
-});
+// window.addEventListener('beforeinstallprompt',function(event){
+//     console.log('beforeinstallprompt fired');
+//     event.preventDefault();
+//     deferredPrompt = event;
+//     return false;    
+// });
 
-var promise = new Promise(function(resolve,reject){
-    setTimeout(function(){
-    //resolve('This is executed once the timer is done!');
-    reject({code:500, message:'An error occurred'})
-    //console.log('this is executed in settimeout');
-},3000);
-});
+// var promise = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//     //resolve('This is executed once the timer is done!');
+//     reject({code:500, message:'An error occurred'})
+//     //console.log('this is executed in settimeout');
+// },3000);
+// });
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://httpbin.org/ip');
-xhr.responseType = 'json';
-xhr.onload = function(){
-    console.log("Where");
-    console.log(xhr.response);
-}
+// var xhr = new XMLHttpRequest();
+// xhr.open('GET', 'https://httpbin.org/ip');
+// xhr.responseType = 'json';
+// xhr.onload = function(){
+//     console.log("Where");
+//     console.log(xhr.response);
+// }
 
-xhr.onerror = function(){
-    console.log('Error!');
-}
+// xhr.onerror = function(){
+//     console.log('Error!');
+// }
 
-xhr.send();
+// xhr.send();
 
-// fetch('https://httpbin.org/ip').then(
+// // fetch('https://httpbin.org/ip').then(
+// //     function(response){console.log(response);
+// //         return response.json();
+    
+// //     }
+// //     ).then(function(data){console.log(data)})
+// //     .catch(function(err){console.log(err)});
+
+
+// fetch('https://httpbin.org/post',{
+//     method: 'POST',
+//     headers:{
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json'
+//     },
+//     mode: 'cors',
+//     body: JSON.stringify({message: 'Does this work?'})
+// }).then(
 //     function(response){console.log(response);
 //         return response.json();
     
@@ -48,38 +65,21 @@ xhr.send();
 //     ).then(function(data){console.log(data)})
 //     .catch(function(err){console.log(err)});
 
-
-fetch('https://httpbin.org/post',{
-    method: 'POST',
-    headers:{
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    mode: 'cors',
-    body: JSON.stringify({message: 'Does this work?'})
-}).then(
-    function(response){console.log(response);
-        return response.json();
-    
-    }
-    ).then(function(data){console.log(data)})
-    .catch(function(err){console.log(err)});
+// // promise.then(
+// //     function(text){return text;},
+// //     function(err){console.log(err);}
+// // ).then(
+// //     function(newText){console.log(newText + "!!!")}
+// // );
 
 // promise.then(
-//     function(text){return text;},
-//     function(err){console.log(err);}
+//     function(text){return text;}    
 // ).then(
 //     function(newText){console.log(newText + "!!!")}
+// ).catch(
+//     function(err){console.log(err);}
 // );
 
-promise.then(
-    function(text){return text;}    
-).then(
-    function(newText){console.log(newText + "!!!")}
-).catch(
-    function(err){console.log(err);}
-);
-
-console.log('This is executed right after timeout');
+// console.log('This is executed right after timeout');
 
 
--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,85 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v4';
+
 self.addEventListener('install',function(event){
     console.log('[Service Worker] Installing Service worker ...',event);
-});
+    event.waitUntil(
+        caches.open(CACHE_STATIC_NAME).then(
+            function(cache){
+                console.log('[Service Worker] Precaching App Shell');
+                
+                //Be careful, if one url is wrong, it seems like the entire
+                //array will not be cached
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]);
+                                
+                // cache.add('/')
+                // cache.add('/index.html')
+                // cache.add('/src/js/app.js')
+                // cache.add('/src/js/feed.js')
+                // cache.add('/src/js/promise.js')
+                // cache.add('/src/js/fetch.js')
+                // cache.add('/src/js/material.min.js')
+                // cache.add('/src/css/app.css')
+                // cache.add('/src/css/feed.css')
+                // cache.add('/src/images/main-image.jpg')
+                // cache.add('https://fonts.googleapis.com/css?family=Roboto:400,700')
+                // cache.add('https://fonts.googleapis.com/icon?family=Material+Icons')
+                // cache.add('https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css')
+            }
+        ))     
+}); 
 
 self.addEventListener('activate',function(event){
     console.log('[Service Worker] Activating Service worker ...',event);
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList){
+                return Promise.all(keyList.map(
+                    function(key){
+                        if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
+                            console.log('[Service Worker] removing old cache', key);
+                            return caches.delete(key);
+                        }
+                    }));
+            })
+    );
     return self.clients.claim();
 });
 
 self.addEventListener('fetch',function(event){
-    console.log('[Service Worker] Fetching something....',event);
-    event.respondWith(fetch(event.request));
-});
+    //console.log('[Service Worker] Fetching something....',event);
+    //event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                if(response){
+                    return response;
+                }else{
+                    return fetch(event.request)
+                    .then(function(res){
+                        return caches.open(CACHE_DYNAMIC_NAME)
+                        .then(function(cache){
+                            cache.put(event.request.url,res.clone());
+                            return res;
+                        })
+                    }).catch(function(err){
+                        
+                    }); 
+                }
+            })
+    );    
+});  
